0c3b458b96db4e0c6ed0270d7f2c152a3b16a76a
YashNagraj75 <yashnagraj75@gmail.com>
2026-05-09 17:50:59 +0530

    Added a few lines to the tokeniser file

--- gpt/tokenizer.py
+++ gpt/tokenizer.py
@@ -1,1 +1,22 @@
 import torch
+import torch.nn as nn
+import rustbpe
+
+
+# Special Tokens used in the dataset, copied from karpathy/nanochat
+SPECIAL_TOKENS = [
+    # every document begins with the Beginning of Sequence (BOS) token that delimits documents
+    "<|bos|>",
+    # tokens below are only used during finetuning to render Conversations into token ids
+    "<|user_start|>",  # user messages
+    "<|user_end|>",
+    "<|assistant_start|>",  # assistant messages
+    "<|assistant_end|>",
+    "<|python_start|>",  # assistant invokes python REPL tool
+    "<|python_end|>",
+    "<|output_start|>",  # python REPL outputs back to assistant
+    "<|output_end|>",
+]
+
+# Again Copy
+SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,2}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
